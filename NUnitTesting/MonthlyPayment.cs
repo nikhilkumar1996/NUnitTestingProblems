5172c50ef9a072ad244993b5b828bfaf312768a6
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-12 23:24:56 +0530

    [Nikhil Kumar]:MonthlyPayment  Program Added

---
+++ NUnitTesting/MonthlyPayment.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    public class MonthlyPayment
+    {
+        public static void Payments(int argument1, int argument2, int argument3)
+        {
+            
+            double principalLoan = Convert.ToDouble(argument1);
+            double year = Convert.ToDouble(argument2);
+            double n = 12 * year;
+            double r = principalLoan / (12 * 100);
+            double percentInterest = Convert.ToDouble(argument3);
+            double payment = (principalLoan * r)/1- Math.Pow(1 + r, -n);
+            Console.WriteLine("Payment="+payment);
+        }
+    }
+}
--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -10,29 +10,42 @@ namespace NUnitTesting
             Console.WriteLine("\n 1. Vending Machine \n" +
                                "\n 2. Days Of Week  \n " +
                                "\n 3. Temperature Conversion \n" +
-                               "\n 4. Exit  ");
+                               "\n 4. Monthly Payment \n" +
+                               "\n 5. Exit  ");
             Console.WriteLine("\n Choose Any Program \n");
-            int n=Convert.ToInt32(Console.ReadLine());
-
-            switch (n)
+            bool flag = true;
+            while (flag)
             {
-                case 1: VendingMachine VM=new VendingMachine();
-                    VM.Notes();
-                    break;
-                case 2: DaysOfWeek DOW=new DaysOfWeek();
-                    DOW.GregorianCalender();
-                    break;
+                int n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        VendingMachine VM = new VendingMachine();
+                        VM.Notes();
+                        break;
+                    case 2:
+                        DaysOfWeek DOW = new DaysOfWeek();
+                        DOW.GregorianCalender();
+                        break;
 
-                case 3: TemperatureConversion Temp = new TemperatureConversion();
-                    Temp.Degrees();
-                    break;
+                    case 3:
+                        TemperatureConversion Temp = new TemperatureConversion();
+                        Temp.Degrees();
+                        break;
+                    case 4:
+                        MonthlyPayment.Payments(3,4,5);
+                        break;
 
-                case 4: Console.WriteLine("Exit");
-                    break;
+                    case 5:
+                        flag= false;
+                        break;
 
-                default: Console.WriteLine("Enter Correct Number");
-                    break;
+                    default:
+                        Console.WriteLine("Enter Correct Number");
+                        break;
+                }
             }
+            
             
         }
     }
